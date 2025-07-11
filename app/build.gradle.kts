0e9cd7d2ec162ca47703af23d333b71be78529fc
Nirjus <karmakarnirjus4839@gmail.com>
2025-07-11 13:53:11 +0530

    Set up and initialize the project, and dependency injection done.

---
+++ app/build.gradle.kts
@@ -0,0 +1,90 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    kotlin("plugin.serialization") version "2.0.0"
+}
+
+android {
+    namespace = "com.example.shopapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.shopapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    // this ia for hillt dependency injection implementation("com.google.dagger:hilt-android:2.50")
+//    ksp("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("com.google.dagger:hilt-android:2.56.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
+
+
+    // this is for coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta06")
+
+    // Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+
+    //this is for pager
+
+    implementation("com.google.accompanist:accompanist-pager:0.28.0")
+
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.28.0")
+
+// this is for payment gateway
+    implementation("com.razorpay:checkout:1.6.40")
+
+
+    //custom bottom nev bar  implementation("com.canopas.compose-animated-navigationbar:bottombar:1.0.1")
+}
