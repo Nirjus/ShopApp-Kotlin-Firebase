465277dd8b1823b71b17a355280ba90d0bda6f18
Nirjus <karmakarnirjus4839@gmail.com>
2025-07-24 23:52:07 +0530

    create CartScreenUi.kt and GetallProductsScreen.kt screen

--- app/src/main/java/com/example/shopapp/presentation/Screens/GetallProductsScreen.kt
+++ app/src/main/java/com/example/shopapp/presentation/Screens/GetallProductsScreen.kt
@@ -1,11 +1,122 @@
 package com.example.shopapp.presentation.Screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.shopapp.presentation.Navigation.Routes
+import com.example.shopapp.presentation.utils.CircularIndicator
+import com.example.shopapp.presentation.utils.ProductItem
+import com.example.shopapp.presentation.viewModels.ShoppingAppViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GetAllProductScreen(
     navController: NavController,
-    ){
+    viewModel: ShoppingAppViewModel = hiltViewModel()
+) {
+    val getAllProductState = viewModel.getAllProductsState.collectAsStateWithLifecycle()
+    val productData = getAllProductState.value.userData
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getAllProducts()
+    }
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "All Products",
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text("Search") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search"
+                    )
+                })
+            when {
+                getAllProductState.value.isLoading -> {
+                    CircularIndicator()
+                }
 
+                getAllProductState.value.errorMessage != null -> {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text("Sorry unable to load products")
+                    }
+                }
+
+                productData.isEmpty() -> {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text("No products available")
+                    }
+                }
+
+                else -> {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                    ) {
+                        items(productData) { product ->
+                            ProductItem(
+                                product!!,
+                                onProductClick = {
+                                    navController.navigate(
+                                        Routes.ProductDetailsScreen(product.productId)
+                                    )
+                                })
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
